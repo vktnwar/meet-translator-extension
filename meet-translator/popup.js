b04ee63a24f9ffa361efcfb2efbaf6463fb8d1b5
Victor Guerra <159503019+vktnwar@users.noreply.github.com>
2025-08-26 03:06:53 +0000

    feat:create a interface files

---
+++ meet-translator/popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const startButton = document.getElementById('start-button');
+  startButton.addEventListener('click', function() {
+    console.log('Botão de iniciar clicado!');
+  });
+});
